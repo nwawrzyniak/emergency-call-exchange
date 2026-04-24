632ab85a0129c66f680835b9587e244a08388662
Nico Wawrzyniak <nwawrzyniak@immopark.de>
2026-04-24 13:32:26 +0200

    Refactored main module

--- main.js
+++ main.js
@@ -3,27 +3,24 @@ import express from 'express';
 
 const app = express();
 
-import configureSecurity from './server/security.js';
-import configureCors from './server/cors.js';
-import configureServeStatic from './server/serve-static.js';
-import configureLimiter from './server/limiter.js';
-import configureRoutes from './server/routes.js';
-import configureHealthCheck from './server/health-check.js';
-import configureAPI from './server/api.js';
-import configureServeIndex from './server/serve-index.js';
-import configureErrorHandling from './server/error-handling.js';
-import connectDatabaseAndStartServer from './server/server.js';
+const modules = [
+  './server/security.js',
+  './server/cors.js',
+  './server/serve-static.js',
+  './server/limiter.js',
+  './server/routes.js',
+  './server/health-check.js',
+  './server/api.js',
+  './server/serve-index.js',
+  './server/error-handling.js',
+];
 
-configureSecurity(app);
-configureCors(app);
-configureServeStatic(app);
-configureLimiter(app);
-configureRoutes(app);
-configureHealthCheck(app);
-configureAPI(app);
-configureServeIndex(app);
-configureErrorHandling(app);
+for (const mod of modules) {
+  const { default: configure } = await import(mod);
+  configure(app);
+}
 
+const { default: connectDatabaseAndStartServer } = await import('./server/server.js');
 connectDatabaseAndStartServer(app);
 
 export default app;
